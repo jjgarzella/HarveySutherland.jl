df0f7c4fed27a97c869305101777d4bf5139f989
Documenter.jl <documenter@juliadocs.github.io>
2026-03-25 06:38:04 +0000

    build based on e569daf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HarveySutherland","page":"Home","title":"HarveySutherland","text":"Documentation for HarveySutherland.\n\n","category":"section"}]
+}
