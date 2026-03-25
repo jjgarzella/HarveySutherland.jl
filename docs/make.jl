e569daf4228a69dce380eea7455b09412114c168
JJ Garzella <jjgarzella@gmail.com>
2026-03-24 23:35:25 -0700

    Add utilities for CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using HarveySutherland
 using Documenter
 
-DocMeta.setdocmeta!(HarveySutherland, :DocTestSetup, :(using HarveySutherland); recursive=true)
+DocMeta.setdocmeta!(
+    HarveySutherland,
+    :DocTestSetup,
+    :(using HarveySutherland);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[HarveySutherland],
-    authors="JJ Garzella <jjgarzella@gmail.com> and contributors",
-    sitename="HarveySutherland.jl",
-    format=Documenter.HTML(;
-        canonical="https://jjgarzella.github.io/HarveySutherland.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [HarveySutherland],
+    authors = "JJ Garzella <jjgarzella@gmail.com> and contributors",
+    sitename = "HarveySutherland.jl",
+    format = Documenter.HTML(;
+        canonical = "https://jjgarzella.github.io/HarveySutherland.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/jjgarzella/HarveySutherland.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/jjgarzella/HarveySutherland.jl", devbranch = "main")
